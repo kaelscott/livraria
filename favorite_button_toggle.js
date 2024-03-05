8c05c9ef19a6602c9af5097bf49116171ed9f1c9
kael <137122131+kaelscott@users.noreply.github.com>
2024-03-05 10:32:22 -0300

    Update favorite_button_toggle.js
    add color toggle

--- favorite_button_toggle.js
+++ favorite_button_toggle.js
@@ -1,8 +1,4 @@
-// Primeiro, obtenha o botão pelo seu ID ou classe
-var botao = document.querySelector('.favButton');
-// Em seguida, adicione um ouvinte de evento 'click' ao botão
+var botao = document.querySelector('.favButton')
 botao.addEventListener('click', function() {
-    // Dentro do manipulador de eventos, use 'classList.toggle' para alternar a classe
-    this.classList.toggle('active');
-
+    this.classList.toggle('active')
 });
